df5de0ee4787c4edf8ef5cec1c4ed57cb73e5937
Enrique Peña Gonzalez <er.penagonzalez@gmail.com>
2014-08-27 10:19:25 -0500

    add Form ABC
    add Form ABC

---
+++ application/modules/main/controllers/FormsController.php
@@ -0,0 +1,61 @@
+<?php
+
+class main_FormsController extends My_Controller_Action
+{
+	protected $_clase = 'mforms';
+	public $validateNumbers;
+	public $validateAlpha;
+	public $dataIn;
+	public $idToUpdate=-1;
+	public $errors = Array();
+	public $operation='';
+	public $resultop=null;	
+		
+    public function init()
+    {
+    	try{
+		$sessions = new My_Controller_Auth();
+		$perfiles = new My_Model_Perfiles();
+        if(!$sessions->validateSession()){
+            $this->_redirect('/');		
+		}
+		$this->view->dataUser   = $sessions->getContentSession();
+		$this->view->modules    = $perfiles->getModules($this->view->dataUser['ID_PERFIL']);
+		$this->view->moduleInfo = $perfiles->getDataModule($this->_clase);
+		
+		$this->dataIn = $this->_request->getParams();
+		$this->validateNumbers = new Zend_Validate_Digits();
+				
+		if(isset($this->dataIn['optReg'])){
+			$this->operation = $this->dataIn['optReg'];
+			
+			if($this->operation=='update'){
+				$this->operation = $this->dataIn['optReg'];
+
+				$this->validateAlpha   = new Zend_Validate_Alnum(array('allowWhiteSpace' => true));				
+			}	
+		}
+		
+		if(isset($this->dataIn['catId']) && $this->validateNumbers->isValid($this->dataIn['catId'])){
+			$this->idToUpdate 	   = $this->dataIn['catId'];	
+		}else{
+			$this->idToUpdate 	   = -1;
+			$this->errors['status'] = 'no-info';
+		}	
+
+		} catch (Zend_Exception $e) {
+            echo "Caught exception: " . get_class($e) . "\n";
+        	echo "Message: " . $e->getMessage() . "\n";                
+        }  		
+    }
+    
+    public function indexAction(){
+    	try{
+			$classObject = new My_Model_Telefonos();
+			$this->view->datatTable = $classObject->getDataTables($this->view->dataUser['ID_EMPRESA']);
+		} catch (Zend_Exception $e) {
+            echo "Caught exception: " . get_class($e) . "\n";
+        	echo "Message: " . $e->getMessage() . "\n";                
+        }  
+    }    
+}
